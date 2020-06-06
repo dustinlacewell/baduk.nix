8bb652451ca7a1aee7298f5e40a6b02766fc8887
Dustin Lacewell <dlacewell@gmail.com>
2020-06-06 01:37:20 -0500

    Fix where sabaki enable option is declared

--- nix/sabaki/default.nix
+++ nix/sabaki/default.nix
@@ -5,7 +5,9 @@ with pkgs.lib;
 let
   cfg = config.baduk.sabaki;
 in {
-  options.baduk.sabaki = mkEnableOption "sabaki";
+  options.baduk.sabaki = {
+    enable = mkEnableOption "sabaki";
+  };
   config = mkIf cfg.enable {
     home.packages = [ pkgs.hello ];
   };
