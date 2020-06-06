4d05b10e0f665cf7cd8701834b6c517b9ef76726
Dustin Lacewell <dlacewell@gmail.com>
2020-06-06 15:50:48 -0500

    Restore code to write out Sabaki config

--- nix/sabaki/default.nix
+++ nix/sabaki/default.nix
@@ -6,10 +6,12 @@ let
   cfg = config.baduk.sabaki;
   options = import ./options.nix { inherit pkgs lib cfg; };
   sabaki = import ./package.nix { inherit pkgs lib cfg; };
+  sabakiConfig = import ./config.nix { inherit pkgs lib cfg; };
 in {
   options.baduk.sabaki = options;
-  config = mkIf cfg.enable {
-    home.packages = [ sabaki ];
-  };
+  config = mkMerge [
+    (mkIf cfg.enable { home.packages = [ sabaki ]; })
+    (mkIf ((length cfg.engines) > 0) { home.activation.sabaki-config = sabakiConfig; })
+  ];
 }
 
